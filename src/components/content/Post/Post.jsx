4fed9bfe6ba612fd4bea3c2b0ca946a5755c2b67
Truong Le <lesytruong247@gmail.com>
2024-10-22 10:45:21 +0700

    split component

--- src/components/content/Post/Post.jsx
+++ src/components/content/Post/Post.jsx
@@ -1,40 +1,57 @@
 import { data } from '../../../data';
 import './index.scss';
 import Card from '../Card/Card';
+import { useRef } from 'react';
+
+const PostItem = ({ data, index }) => {
+  return (
+    <div key={index} className='container'>
+      <div
+        style={{
+          display: 'flex',
+        }}
+      >
+        <div>
+          <Card cover={data?.cover} />
+        </div>
+
+        <div className='line' />
+        <div className='content'>
+          <div className='header-content'>
+            <div>
+              <p className='title'>{data?.title}</p>
+            </div>
+            <div>
+              <p className='sub-title'>{data?.content[0]}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+PostItem.propTypes = {
+  data: Object,
+  index: Number,
+};
 
 const Post = () => {
+  const limitPosts = useRef(3);
   return (
     <>
       <header className='title-header'>
         <h1>HOẠT ĐỘNG NỔI BẬT</h1>
       </header>
-      <div>
-        {data.map((data, index) => (
-          <div key={index} className='container'>
-            <div
-              style={{
-                display: 'flex',
-              }}
-            >
-              <div>
-                <Card cover={data?.cover} />
-              </div>
-
-              <div className='line' />
-              <div className='content'>
-                <div className='header-content'>
-                  <div>
-                    <p className='title'>{data?.title}</p>
-                  </div>
-                  <div>
-                    <p className='sub-title'>{data?.content[0]}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      <section className='section-1'>
+        {data.map((data, index) => {
+          if (index >= limitPosts.current) return null;
+          return <PostItem data={data} key={index} index={index} />;
+        })}
+      </section>
+      <section className='section-2'>
+        <div className='image'></div>
+      </section>
     </>
   );
 };
